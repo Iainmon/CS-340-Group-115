184ec905e9c6fea1a508db4150162be8bc649c88
Iain Moncrief <Iainmoncrief@gmail.com>
2023-02-16 09:52:02 -1000

    hard queries done

--- Step 3/DMQ.sql
+++ Step 3/DMQ.sql
@@ -12,17 +12,39 @@ select * from medications;
 
 -- Populating prescriptions list
 select 
-    prescription.prescription_id,
-    prescription_status.status,
-    prescription_status.update_date,
-    customer.first_name as customer_first_name,
-    customer.last_name as customer_last_name,
-    medication.name as medication_name,
-    prescription.dosage,
-    prescription.refill_count,
-    prescription.refill_frequency
-from prescription 
-    inner join prescription_status on prescription.prescription_id = prescription_status.prescription_id
-    inner join customer on prescription.customer_id = customer.customer_id
-    inner join medication on prescription.medication_id = medication.medication_id;
+    p.prescription_id,
+    COALESCE(ps.status, NULL) as last_status,
+    COALESCE(ps.update_date, NULL) as update_date,
+    c.first_name as customer_first_name,
+    c.last_name as customer_last_name,
+    m.name as medication_name,
+    p.dosage,
+    p.refill_count,
+    p.refill_frequency
+from prescriptions p
+left join (
+    select prescription_id, MAX(update_date) as latest_date
+    from prescription_status
+    group BY prescription_id
+) as latest_status
+on p.prescription_id = latest_status.prescription_id
+left join prescription_status ps
+on ps.prescription_id = latest_status.prescription_id and ps.update_date = latest_status.latest_date
+join customers c on p.customer_id = c.customer_id
+join medications m on p.medication_id = m.medication_id
+group by p.prescription_id;
+
+
+
+
+select 
+    ps.prescription_id,
+    ps.status,
+    ph.first_name as pharmacist_first_name,
+    ph.last_name as pharmacist_last_name,
+    ps.update_date
+from prescription_status ps left join pharmacists ph on ps.pharmacist_id = ph.pharmacist_id
+order by ps.update_date desc;
+
+
 
