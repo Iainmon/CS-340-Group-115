57938c3b2d070af0e8d4dd2b0e72d835e889c2ca
Iain Moncrief <Iainmoncrief@gmail.com>
2023-03-02 18:42:17 -1000

    working. there will be a lot of these.

--- site/frontend/src/fetcher.js
+++ site/frontend/src/fetcher.js
@@ -6,12 +6,17 @@ const backendURL = 'http://flip1.engr.oregonstate.edu:' + port;
 export async function primeTables() {
     console.log('Fetching tables...');
     const promises = Object.keys(tables).map(async tableName => {
-        const response = await fetch(backendURL + '/populate/' + tableName);
-        const json = await response.json();
-        console.log('Fetched table:', tableName);
-        console.log(json);
-        tables[tableName] = json;
-        return json;
+        try {
+            const response = await fetch(backendURL + '/populate/' + tableName);
+            const json = await response.json();
+            console.log('Fetched table:', tableName);
+            console.log(json);
+            tables[tableName] = json;
+            return json;
+        } catch (e) {
+            console.error('Error fetching table:', tableName);
+            console.error(e);
+        }
     });
     const results = await Promise.all(promises);
     console.log(results);
