f2a11dded4d023d0b83d276d3f1cd3703e310975
Iain Moncrief <Iainmoncrief@gmail.com>
2023-03-02 18:51:27 -1000

    working. there will be a lot of these.

--- site/backend/app.js
+++ site/backend/app.js
@@ -14,7 +14,7 @@ const { populate } = require('./manipulator');
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
