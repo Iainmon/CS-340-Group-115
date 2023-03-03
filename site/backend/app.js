3eddf0c3ed614d9ff50923300f8230bfb432cb0c
Iain Moncrief <Iainmoncrief@gmail.com>
2023-03-02 19:52:04 -1000

    working. there will be a lot of these.TEST

--- site/backend/app.js
+++ site/backend/app.js
@@ -4,6 +4,8 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+const mysql = require('mysql');
+
 PORT        = 2235;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
